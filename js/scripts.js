b99e3d19c2f82d57770a16d31071a1c50c88f897
tommy bountham <bvone@ymail.com>
2015-04-17 11:24:25 -0700

    fix bud on spec

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
  var Pizza = {
     pizzaType: [],
-    pizzCost: 10,
+    pizzaCost: 20,
     Cheese: function() {
         if (this.pizzaType.indexOf("cheese") !== -1 ) {
         return true;
@@ -11,11 +11,12 @@
 
     pizzaTotal: function() {
         if (this.pizzaSize === 10) {
-        this.pizzCost += 5;
-      } else if (this.pizzaSize === 12) {
-        this.pizzCost += 7;
-        }
-        return this.pizzCost;
+        this.pizzaCost += 5;
+      }
+       else if (this.pizzaSize === 12) {
+        this.pizzaCost += 7;
+      }
+        return this.pizzaCost;
       }
     };
 
@@ -25,7 +26,6 @@ $(function(){
     event.preventDefault();
     var inputtedSize = parseInt($("input[name=pizza-size]:checked").val());
     var inputtedPizzaType = $("input[name=pizzaType]:checked").val();
-    alert(inputtedSize);
     var newPizzaOrder = Object.create(Pizza);
 
     newPizzaOrder.pizzaSize = inputtedSize;
