9083db2fc26d4cfff6d537ae520b0026a1a3f3bd
tommy bountham <bvone@ymail.com>
2015-04-17 11:00:33 -0700

    working interface and total cost pizza

--- js/scripts.js
+++ js/scripts.js
@@ -16,5 +16,24 @@
         this.pizzCost += 7;
         }
         return this.pizzCost;
-    }
-  };
+      }
+    };
+
+
+$(function(){
+    $("form#new-list").submit(function(event) {
+    event.preventDefault();
+    var inputtedSize = parseInt($("input[name=pizza-size]:checked").val());
+    var inputtedPizzaType = $("input[name=pizzaType]:checked").val();
+    alert(inputtedSize);
+    var newPizzaOrder = Object.create(Pizza);
+
+    newPizzaOrder.pizzaSize = inputtedSize;
+    newPizzaOrder.pizzaType.push(inputtedPizzaType);
+
+    $(".total-cost").text(newPizzaOrder.pizzaTotal());
+
+    $("#show-price").show();
+
+});
+});
